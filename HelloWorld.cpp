0b52620d62c1fc8d6fb8ddd980cbac68edc20794
phantomfancy <89564432+phantomfancy@users.noreply.github.com>
2025-08-14 10:49:05 +0800

    test github repo in 2025/8/14.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,18 @@
+/**
+ * @file HelloWorld.cpp
+ * @author colin liu (changle.liu@chipsemicorp.com)
+ * @brief This is a simple HelloWorld program in C++.I write this code to test my git repository in github.
+ * @version 0.1
+ * @date 2025/8/14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include <iostream>
 
 int main()
 {
-    std::cout >> "HelloWorld!" << std::endl;
+    std::cout << "HelloWorld!" << std::endl;    
+    std::cout << "Testgit repo on 2025/8/14" << std::endl;
     return 0;
 }
