4e8893e9d7d03a3938ed83a37ef42f7dfd4c6761
phantomfancy <89564432+phantomfancy@users.noreply.github.com>
2025-08-14 11:16:09 +0800

    test on visual studio

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * @file HelloWorld.cpp
  * @author colin liu (changle.liu@chipsemicorp.com)
  * @brief This is a simple HelloWorld program in C++.I write this code to test my git repository in github.
@@ -12,7 +12,8 @@
 
 int main()
 {
-    std::cout << "HelloWorld!" << std::endl;    
-    std::cout << "Testgit repo on 2025/8/14" << std::endl;
+    std::cout << "HelloWorld!" << std::endl;
+    std::cout << "Test git repo on 2025/8/14 using VSCode" << std::endl;
+    std::cout << "Also test on Visual Studio" << std::endl;
     return 0;
 }
