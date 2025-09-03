c9ce849253c2a39198388d9bd293fd524ec75fef
phantomfancy <89564432+phantomfancy@users.noreply.github.com>
2025-09-03 10:26:25 +0800

    update .gitinore file.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,7 @@
  * @author colin liu (changle.liu@chipsemicorp.com)
  * @brief This is a simple HelloWorld program in C++.I write this code to test my git repository in github.
  * @version 0.1
- * @date 2025/8/14
+ * @date 2025/9/3
  * 
  * @copyright Copyright (c) 2025
  * 
